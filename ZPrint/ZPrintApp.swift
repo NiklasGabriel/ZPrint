a91f96ad3cb772cbb6ed889e92e2591988f370b1
Niklas Gabriel <nikgab@gmx.de>
2026-07-08 14:06:22 +0200

    Initial Commit

---
+++ ZPrint/ZPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZPrintApp.swift
+//  ZPrint
+//
+//  Created by Niklas Gabriel on 08.07.26.
+//
+
+import SwiftUI
+
+@main
+struct ZPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
